9558c20f4e7df583041ffc471b89e01004ec59f0
valiksinev <valiksinev@yahoo.com>
2023-11-11 21:26:09 +0400

    search in depth

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+// https://coderun.yandex.ru/problem/search-in-depth?currentPage=1&pageSize=10&rowNumber=7
+
+// Дан неориентированный граф, возможно, с петлями и кратными ребрами. Необходимо построить компоненту связности, содержащую первую вершину.
+
+// Формат ввода
+// В первой строке записаны два целых числа N (1<=N<=1000)и M (0<=M<=5*100000)— количество вершин и ребер в графе. В последующих M строках перечислены ребра — пары чисел, определяющие номера вершин, которые соединяют ребра.
+
+// Формат вывода
+// В первую строку выходного файла выведите число K — количество вершин в компоненте связности. Во вторую строку выведите
+// K целых чисел — вершины компоненты связности, перечисленные в порядке возрастания номеров.
+
+// Пример 1
+// Ввод     Вывод
+// 4 5      4
+// 2 2      1 2 3 4
+// 3 4
+// 2 3
+// 1 3
+// 2 4
+
+
+use std::io;
+use std::collections::{BTreeMap, BTreeSet};
+
+fn dfs(vertices: &Vec<u32>, set: &mut BTreeSet<u32>, map: &BTreeMap<u32, Vec<u32>> ) {
+    for v in vertices {
+        if set.insert(*v) {
+            dfs(map.get(v).unwrap(), set, map);
+        }
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+    let params: Vec<&str> = s.split(' ').collect();
+
+    let _n: u32 = params.get(0).unwrap().trim().parse().unwrap();
+    let m: u32 = params.get(1).unwrap().trim().parse().unwrap();
+
+    let mut map : BTreeMap<u32, Vec<u32>> = BTreeMap::new();
+    for _i in 0..m {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+        let edges : Vec<&str> = s.split(' ').collect();
+        let u: u32 = edges.get(0).unwrap().trim().parse().unwrap();
+        let v: u32 = edges.get(1).unwrap().trim().parse().unwrap();
+        let mut push = |a, b| {
+            let vertices = map.entry(a).or_insert(vec![]);
+            vertices.push(b);
+        };
+        push(u, v);
+        push(v, u);
+    }
+
+    let mut set = BTreeSet::new();
+
+    if let Some (vertices) = map.get(&1) {
+        set.insert(1_u32);
+        dfs(vertices, &mut set, &map);
+    }
+
+    println!("{}", set.len());
+    let s = format!("{:?}", set).replace("{", "").replace("}", "").replace(",", "");
+    println!("{}", s);
+}
+
+
